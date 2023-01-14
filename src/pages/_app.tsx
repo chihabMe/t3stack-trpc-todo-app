89018c51f7562b7e7b98c48db8c0fd15a7bc2aa9
chihabMg <chihab.mg.me@gmail.com>
2023-01-14 10:47:14 +0100

    added next-prograss-bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import NextNProgress from "nextjs-progressbar";
 
 import { api } from "../utils/api";
 
@@ -13,6 +14,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <NextNProgress color="#5014B8" />
       <Component {...pageProps} />
     </SessionProvider>
   );
